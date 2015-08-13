3734da87e013a76f710f23ffd716ae2588de57de
Aruna <aruna@xiaoma.cn>
2015-08-13 09:34:27 +0800

    add animation class

---
+++ animation.js
@@ -0,0 +1,77 @@
+var Animation = function(parm){
+	if (!parm || !parm.stage) {
+		console.error("Animation 初始化参数无效！");
+		return;
+	}
+	var self = this;
+	this.stage = parm.stage;
+	this.source = {
+		png: parm.img,
+		plist: parm.plist
+	};
+	this.cutX = parm.cutX;
+	this.cutY = parm.cutY;
+	this.width = parm.width;
+	this.height = parm.height;
+	this.x = parm.x;
+	this.y = parm.y;
+	this.imgWidth = parm.imgWidth;
+	this.imgHeight = parm.imgHeight;
+	this.isStart = false;
+	console.log(this);
+	this.init = function(parm){
+		self.stage = parm.stage;
+		var ctx = self.stage.getContext("2d");
+		ctx.fillStyle = "#000000";
+		ctx.fillRect(0, 0, self.stage.width, self.stage.height);
+		ctx.fillStyle = "#333333";
+		ctx.fillRect(self.stage.width/3, self.stage.height/3, self.stage.width/3, self.stage.height/3);
+		self.node = {};
+		self.img = new Image();
+		self.img.src = self.source.png;
+		self.img.onload = function(){
+			ctx.drawImage(/*规定要使用的图像、画布或视频。*/self.img,
+						/*可选。开始剪切的 x 坐标位置。*/ parm.cutX,
+						/*可选。开始剪切的 y 坐标位置。*/parm.cutY,
+						/*可选。被剪切图像的宽度。*/parm.width,
+						/*可选。被剪切图像的高度。*/parm.height,
+						/*在画布上放置图像的 x 坐标位置。*/parm.x,
+						/*在画布上放置图像的 y 坐标位置。*/parm.y,
+						/*可选。要使用的图像的宽度。（伸展或缩小图像）*/parm.imgWidth,
+						/*可选。要使用的图像的高度。（伸展或缩小图像）*/parm.imgHeight);
+		};
+		console.log(self);
+	};
+	this.play = function(repeat){
+		self.isStart = true;
+		var drawImage = function(i){
+			var imgIndex = i%4;
+			var ctx = self.stage.getContext("2d");
+			ctx.clearRect(0,0,self.stage.width,self.stage.height);
+
+			ctx.drawImage(/*规定要使用的图像、画布或视频。*/self.img,
+						/*可选。开始剪切的 x 坐标位置。*/ self.cutX,
+						/*可选。开始剪切的 y 坐标位置。*/self.cutY+imgIndex*105,
+						/*可选。被剪切图像的宽度。*/self.width,
+						/*可选。被剪切图像的高度。*/self.height,
+						/*在画布上放置图像的 x 坐标位置。*/self.x+i*10,
+						/*在画布上放置图像的 y 坐标位置。*/self.y+i*5,
+						/*可选。要使用的图像的宽度。（伸展或缩小图像）*/self.imgWidth,
+						/*可选。要使用的图像的高度。（伸展或缩小图像）*/self.imgHeight);
+		};
+		var count = 0;
+		if(repeat){
+			self.timer = setInterval(function(){
+				if(!self.isStart){
+					clearInterval(self.timer);
+					return;
+				}
+				drawImage(count ++);
+			},100)
+		}
+	};
+	this.stop = function(){
+		self.isStart = false;
+	};
+	this.init(parm);
+};
--- main.js
+++ main.js
@@ -1,38 +1,9 @@
-var canvas = null;
-var img = null;
-var ctx = null;
-var resize = function(){
-	canvas.width = canvas.parentNode.clientWidth;;
-	canvas.height = canvas.parentNode.clientHeight;
-	img = new Image();
-	img.src = "store.png";
-	img.onload = function(){
-		console.log(img.width);
-		console.log(img.height);
-		ctx.drawImage(/*规定要使用的图像、画布或视频。*/img,
-					/*可选。开始剪切的 x 坐标位置。*/ 140,
-					/*可选。开始剪切的 y 坐标位置。*/0,
-					/*可选。被剪切图像的宽度。*/110,
-					/*可选。被剪切图像的高度。*/105,
-					/*在画布上放置图像的 x 坐标位置。*/(canvas.width-img.width)/2,
-					/*在画布上放置图像的 y 坐标位置。*/(canvas.height-img.height)/2,
-					/*可选。要使用的图像的宽度。（伸展或缩小图像）*/100,
-					/*可选。要使用的图像的高度。（伸展或缩小图像）*/100);//canvas.width/2 – (img.width/2), canvas.height/2 – (img.height/2)
-	};
-	ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = "#333333";
-	ctx.fillRect(canvas.width/3, canvas.height/3, canvas.width/3, canvas.height/3);
-	console.log(ctx);
-
-	// ctx.drawImage(img, canvas.width/2,canvas.height/2);//canvas.width/2 – (img.width/2), canvas.height/2 – (img.height/2)
-};
 window.onload = function(){
-	canvas = document.getElementById("stage");
+	var canvas = document.getElementById("stage");
+	canvas.width = canvas.parentNode.clientWidth;;
+	canvas.height = 500;
 	var playButton = document.getElementById("play");
 	var stopButton = document.getElementById("stop");
-	var moveButton = document.getElementById("move");
 	var parm = {
 		stage: canvas,
 		img: "store.png",
@@ -47,15 +18,14 @@ window.onload = function(){
 		imgHeight: 100
 	};
 	var animation = new Animation(parm);
-	playButton.addEventListener("click",function(){
-		console.log("play");
-		console.log(animation);
-
+	playButton.addEventListener("click", function(){
+		playButton.style.display = "none";
+		stopButton.style.display = "block";
 		animation.play(true);
 	});
-	stopButton.addEventListener("click",function(){
-		console.log("stop");
-		console.log(animation);
+	stopButton.addEventListener("click", function(){
+		stopButton.style.display = "none";
+		playButton.style.display = "block";
 		animation.stop();
 	});
 };
